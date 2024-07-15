84ac23de4fb9832c31fd01383702b2bedf60762e
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-07-15 13:46:47 +0000

    run artillery in github

--- artillery/helper/auth.mjs
+++ artillery/helper/auth.mjs
@@ -5,23 +5,24 @@ import axios from "axios"
 import {dirname} from "path"
 import {fileURLToPath} from "url"
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+// const __dirname = dirname(fileURLToPath(import.meta.url))
 
-const privateKey = fs.readFileSync(`${__dirname}/private.key`, "utf8")
+// const privateKey = fs.readFileSync(`${__dirname}/private.key`, "utf8")
+const privateKey = process.env.psu_private_key
 const auth_url = "https://internal-dev.api.service.nhs.uk/oauth2/token"
 
 function createSignedJWT() {
   const header = {
     typ: "JWT",
     alg: "RS512",
-    kid: "psu-internal-dev"
+    kid: process.env.psu_kid
   }
   const jti_value = uuidv4()
 
   const currentTimestamp = Math.floor(Date.now() / 1000)
   const data = {
-    sub: process.env.API_KEY,
-    iss: process.env.API_KEY,
+    sub: process.env.psu_api_key,
+    iss: process.env.psu_api_key,
     jti: jti_value,
     aud: auth_url,
     exp: currentTimestamp + 180 // expiry time is 180 seconds from time of creation
@@ -31,13 +32,22 @@ function createSignedJWT() {
   return signedJWT
 }
 
-export async function getAccessToken() {
+export async function getAccessToken(logger) {
   const signedJWT = createSignedJWT()
   const payload = {
     grant_type: "client_credentials",
     client_assertion_type: "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
     client_assertion: signedJWT
   }
-  const response = await axios.post(auth_url, payload, {headers: {"content-type": "application/x-www-form-urlencoded"}})
-  return response.data
+  try {
+    const response = await axios.post(auth_url, payload, {
+      headers: {"content-type": "application/x-www-form-urlencoded"}
+    })
+    logger.info(`response data: ${JSON.stringify(response.data)}`)
+    return response.data
+  } catch (error) {
+    logger.error("Got an error")
+    logger.error(error.response)
+    throw error
+  }
 }
--- scripts/run_psu_load_test.sh
+++ scripts/run_psu_load_test.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
-if [ -z "${API_KEY}" ]; then
-    echo "API_KEY is unset or set to the empty string"
-    exit 1
-fi
+security_group=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "vpc-resources:VpcDefaultSecurityGroup") | .Value' | grep -o '[^:]*$')
+export security_group="sg-06084b206aa260b31"
+vpc_subnets=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "vpc-resources:PrivateSubnets") | .Value' | grep -o '[^:]*$')
+export vpc_subnets
 
-artillery run artillery/psu_load_test.yml
+artillery_worker_role_name=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "artillery-resources:ArtilleryWorkerRoleName") | .Value' | grep -o '[^:]*$')
+export artillery_worker_role_name
+
+
+#artillery run artillery/psu_load_test.yml
+artillery run-fargate \
+    --secret psu_api_key \
+    --secret psu_private_key \
+    --secret psu_kid \
+    --region eu-west-2 \
+    --security-group-ids ${security_group} \
+    --subnet-ids ${vpc_subnets} \
+    --task-role-name ${artillery_worker_role_name} \
+    artillery/psu_load_test.yml
